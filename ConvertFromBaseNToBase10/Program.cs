72917d2950e1f55b004f1b16228e26c98fc50703
Slav Tomov <stomow@outlook.com>
2022-05-06 16:30:31 +0300

    Add Unicode Characters

---
+++ ConvertFromBaseNToBase10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace ConvertFromBaseNToBase10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var nums = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int numLength = nums[1].Length;
+
+            int nBase = int.Parse(nums[0]);
+            var num = nums[1].ToCharArray();
+            Array.Reverse(num);
+
+            BigInteger result = new BigInteger();
+            if (nBase >= 2 && nBase <= 10)
+            {
+                for (int i = 0; i < numLength; i++)
+                {
+                    int currDigit = int.Parse(num[i].ToString());
+                    BigInteger currMultiplier = BigInteger.Pow(nBase, i);
+                    result += currMultiplier * currDigit;
+                }
+                Console.WriteLine(result);
+                return;
+            }
+            Console.WriteLine(0);
+        }
+    }
+}
---
+++ UnicodeCharacters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace UnicodeCharacters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in input)
+                sb.AppendFormat("\\u{0:x4}", (int)c);
+
+            Console.WriteLine(sb);
+        }
+    }
+}
