a9a21f3512884ba8869bbf15e0ac63f9356a36c1
slavCode <stomow@gmail.com>
2022-05-09 16:33:05 +0300

    Add Valid Usernames

---
+++ ValidUsernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ValidUsernames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            string pattern = @"\b[a-zA-Z]([a-zA-Z0-9_]){2,25}\b";
+
+            var matches = Regex.Matches(input, pattern);
+
+            int longestLen = 0;
+            Dictionary<string, string> usernames = new Dictionary<string, string>();
+            for (int i = 1; i < matches.Count; i++)
+            {
+
+                int currLen = matches[i - 1].Length + matches[i].Length;
+                if (currLen > longestLen)
+                {
+                    longestLen = currLen;
+                    usernames.Clear();
+                    usernames.Add(matches[i - 1].Value, matches[i].Value);
+                }
+            }
+
+            Console.WriteLine(usernames.Keys.First());
+            Console.WriteLine(usernames.Values.First());
+        }
+    }
+}
