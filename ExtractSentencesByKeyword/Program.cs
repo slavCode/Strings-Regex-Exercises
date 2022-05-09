9da234f275e0ad06957da6d353507ac40de2d946
slavCode <stomow@gmail.com>
2022-05-09 15:19:57 +0300

    Add Extract sentences by keyword

---
+++ ExtractSentencesByKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentencesByKeyword
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine().Trim();
+            var lines = Console.ReadLine().Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            string pattern = $@"(?<=\W){word}(?=\W)";
+
+            foreach (var line in lines)
+            {
+                bool isMatch = Regex.IsMatch(line, pattern);
+                if (isMatch)
+                {
+                    Console.WriteLine(line.Trim());
+                }
+            }
+        }
+    }
+}
