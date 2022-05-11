f105192c116d29913a51a66678cb2f5b25d0f516
slavCode <stomow@gmail.com>
2022-05-11 13:13:49 +0300

    Add UseYourChainsBuddy and Querry mess

---
+++ QueryMess/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace QueryMess
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string replaced = ReplaceEncoded(input);
+                var pairs = replaced.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(t => t.Trim()).ToArray();
+                var dataDict = new Dictionary<string, string>();
+
+                foreach (var pair in pairs)
+                {
+                    string key = pair.Split('=')[0].Trim();
+                    int keyIdx = key.LastIndexOf('?');
+                    if (keyIdx > -1) key = key.Remove(0, keyIdx + 1);
+
+                    string value = pair.Split('=')[1].Trim();
+                    int valueIdx = value.IndexOf('?');
+                    if (valueIdx > -1) value = value.Remove(valueIdx);
+
+                    if (dataDict.ContainsKey(key))
+                    {
+                        if (!dataDict[key].Contains(value))
+                        {
+                            dataDict[key] += ", " + value;
+                        }
+                        else dataDict[key] = value;
+                    }
+                    else dataDict.Add(key, value);
+                }
+
+                foreach (var pair in dataDict)
+                {
+                    Console.Write($"{pair.Key}=[{pair.Value}]");
+                }
+                Console.WriteLine();
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static string ReplaceEncoded(string input)
+        {
+            string pattern = @"(\+|%20)+";
+
+            return Regex.Replace(input, pattern, " ");
+        }
+    }
+}
---
+++ UseYourChainsBuddy/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UseYourChainsBuddy
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            string pattern = @"(?<=<p>).*?(?=<\/p>)";
+            string input = Console.ReadLine();
+
+            var matches = Regex.Matches(input, pattern);
+            string text = String.Empty;
+            foreach (Match match in matches)
+            {
+                text += Regex.Replace(match.Value, @"[^a-z0-9]+", " ");
+            }
+
+            Dictionary<char, char> replacments = new Dictionary<char, char>();
+            AddDecryption(replacments);
+            StringBuilder result = new StringBuilder();
+            foreach (var letter in text.ToCharArray())
+            {
+                if (Char.IsLetter(letter))
+                {
+                    result.Append(replacments[letter]);
+                }
+                else result.Append(letter);
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+
+        static void AddDecryption(Dictionary<char, char> replacments)
+        {
+            replacments.Add('a', 'n');
+            replacments.Add('b', 'o');
+            replacments.Add('c', 'p');
+            replacments.Add('d', 'q');
+            replacments.Add('e', 'r');
+            replacments.Add('f', 's');
+            replacments.Add('g', 't');
+            replacments.Add('h', 'u');
+            replacments.Add('i', 'v');
+            replacments.Add('j', 'w');
+            replacments.Add('k', 'x');
+            replacments.Add('l', 'y');
+            replacments.Add('m', 'z');
+            replacments.Add('n', 'a');
+            replacments.Add('o', 'b');
+            replacments.Add('p', 'c');
+            replacments.Add('q', 'd');
+            replacments.Add('r', 'e');
+            replacments.Add('s', 'f');
+            replacments.Add('t', 'g');
+            replacments.Add('u', 'h');
+            replacments.Add('v', 'i');
+            replacments.Add('w', 'j');
+            replacments.Add('x', 'k');
+            replacments.Add('y', 'l');
+            replacments.Add('z', 'm');
+        }
+    }
+}
